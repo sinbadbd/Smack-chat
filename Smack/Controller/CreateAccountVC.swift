9225f6d46250321a9609111d3bf96a17424bb14c
imran <imufun1@gmail.cm>
2017-11-14 18:54:44 -0800

    alamofire add

--- Smack/Controller/CreateAccountVC.swift
+++ Smack/Controller/CreateAccountVC.swift
@@ -10,21 +10,46 @@ import UIKit
 
 class CreateAccountVC: UIViewController {
 
+    
+    @IBOutlet weak var usernameTxt: UITextField!
+    @IBOutlet weak var emailTxt: UITextField!
+    @IBOutlet weak var passTxt: UITextField!
+    
+    
+    @IBOutlet weak var userImg: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-
-    @IBOutlet weak var closeAccPressed: UIButton!
+ 
+    @IBAction func CreateAccount(_ sender: Any) {
+        guard let email = emailTxt.text , emailTxt.text != " " else {
+            return
+        }
+        guard let pass = passTxt.text , passTxt.text != " " else {
+            return
+        }
+        
+        AuthService.instance.registerUser(email: email, password: pass) { (success  ) in
+            if success {
+                print("register User")
+            }
+        }
+        
+    }
+    @IBAction func pickAavatarPressed(_ sender: Any) {
+    }
+    
+    
+    @IBAction func pickBGColorPressed(_ sender: Any) {
+    }
     
     @IBAction func CLoseCreateAccount(_ sender: Any) {
         performSegue(withIdentifier: UNWINE, sender: nil)
     }
- 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-    }
+  
 
 
 }
