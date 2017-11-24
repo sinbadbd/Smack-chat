6aee80e5ddcbb00b78aa9b88b0acf5b68aa9f57b
imran <imufun1@gmail.cm>
2017-11-23 22:30:11 -0800

    - Profile modal create with xib file

---
+++ Smack/Controller/ProfileVCViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ProfileVCViewController.swift
+//  Smack
+//
+//  Created by imran on 11/23/17.
+//  Copyright © 2017 Jonny B. All rights reserved.
+//
+
+import UIKit
+
+class ProfileVCViewController: UIViewController {
+
+    
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var userName: UILabel!
+    @IBOutlet weak var userEmail: UILabel!
+    @IBOutlet weak var bgView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    @IBAction func closeModalPressed(_ sender: Any) {
+    }
+    
+    @IBAction func logoutPressed(_ sender: Any) {
+    }
+    
+    
+    
+}
