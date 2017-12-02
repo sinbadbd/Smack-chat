cbed14097fd94b73b734775c5a815cbb30f01f54
imran <imufun1@gmail.cm>
2017-12-02 04:02:33 -0800

    - login refactor ***** - * Worked with notification - channel add and animation -

--- Smack/Controller/ChannelVC.swift
+++ Smack/Controller/ChannelVC.swift
@@ -30,6 +30,10 @@ class ChannelVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
         //Notification
         NotificationCenter.default.addObserver(self, selector: #selector(ChannelVC.userDataDidChange(_:)), name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(channelsLoaded(_:)), name: NOTIF_CHANNELS_LOADED, object: nil)
+        
+        
         SocketServices.instance.getChannel { (success) in
             if success {
                 self.tableView.reloadData()
@@ -38,7 +42,7 @@ class ChannelVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     }
     
     override func viewDidAppear(_ animated: Bool) {
-      //  setUserInfo()
+        setupUserInfo()
     }
     
     @IBAction func LoginBtnPressed(_ sender: Any) {
@@ -54,26 +58,43 @@ class ChannelVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         }
         
     }
-    
-    //User loggedin then notitify. And change image & user email.
-    @objc func userDataDidChange(_ notif: Notification){
-        if AuthService.instance.isLoggedIn{
-            loginBtn.setTitle(UserDataService.instance.name, for: .normal)
-            userImg.image = UIImage(named: UserDataService.instance.avatarName)
-            userImg.backgroundColor = UserDataService.instance.returnUIColor(components: UserDataService.instance.avatarColor
-            )
+    // Add channel
+    @IBAction func addChannel(_ sender: Any) {
+        if AuthService.instance.isLoggedIn {
+            let addChannel = AddchannelVC()
+            addChannel.modalPresentationStyle = .custom
+            present(addChannel, animated: true, completion: nil)
         } else {
-            loginBtn.setTitle("Login", for: .normal)
-            userImg.image = UIImage(named: "menuProfileIcon")
-            //userImg.backgroundColor = UIColor.clear
+            ToastMessage()
         }
     }
     
-    @IBAction func addChannel(_ sender: Any) {
-        let addChannel = AddchannelVC()
-        addChannel.modalPresentationStyle = .custom
-        present(addChannel, animated: true, completion: nil)
+    //User loggedin then notitify. And change image & user email.
+    @objc func userDataDidChange(_ notif: Notification){
+        setupUserInfo()
     }
+    // Notify channel
+    @objc func channelsLoaded(_ notif: Notification){
+        tableView.reloadData()
+    }
+    
+    
+    
+    func setupUserInfo(){
+        if AuthService.instance.isLoggedIn {
+            loginBtn.setTitle(UserDataService.instance.name, for: .normal)
+            userImg.image = UIImage(named: UserDataService.instance.avatarName)
+            userImg.backgroundColor = UserDataService.instance.returnUIColor(components: UserDataService.instance.avatarColor)
+        } else {
+            loginBtn.setTitle("Login", for: .normal)
+            userImg.image = UIImage(named: "menuProfileIcon")
+            userImg.backgroundColor = UIColor.clear
+            tableView.reloadData()
+        }
+    }
+    
+    
+    //
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if let cell = tableView.dequeueReusableCell(withIdentifier: "channelCell", for: indexPath) as? ChannelCell {
@@ -93,21 +114,19 @@ class ChannelVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         return MesageService.instance.channels.count
     }
     
+    func ToastMessage(){
+        let alert = UIAlertController(title: "Alert", message: "Please login or signup", preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "Click", style: UIAlertActionStyle.default, handler: nil)) 
+        self.present(alert, animated: true, completion: nil)
+    }
     
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let channel = MesageService.instance.channels[indexPath.row]
+        MesageService.instance.selectedChannel = channel
+        NotificationCenter.default.post(name: NOTIF_CHANNELS_SELECTED, object: nil)
+        self.revealViewController().revealToggle(animated: true)
+    }
     
     
     
--- Smack/Services/MesageService.swift
+++ Smack/Services/MesageService.swift
@@ -15,6 +15,8 @@ class MesageService {
     
     var channels = [Channel]()
     
+    //Selected current channel
+    var selectedChannel : Channel?
     
     func findAllChannel(completion: @escaping CompletionHandler)  {
         Alamofire.request(URL_GET_CHANNELS, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: BEARER_HEADER).responseJSON { (response) in
@@ -29,6 +31,8 @@ class MesageService {
                         self.channels.append(channel)
                     }
                    print(self.channels)
+                    
+                    NotificationCenter.default.post(name: NOTIF_CHANNELS_LOADED, object: nil)
                     completion(true)
                 }
             } else {
@@ -36,6 +40,11 @@ class MesageService {
                 debugPrint(response.result.error as Any)
             }
         }
+    }
+    
+    //When - User logout then all channel are remove
+    func clearChannels(){
+        channels.removeAll()
     }
     
 }
